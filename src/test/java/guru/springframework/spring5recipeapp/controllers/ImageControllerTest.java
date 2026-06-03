a7106287a72c984b130019ec8ec8459d467c8f3a
gdarity98 <gdarity98@gmail.com>
2026-06-03 13:58:51 -0600

    Refactor Recipe Service

--- src/test/java/guru/springframework/spring5recipeapp/controllers/ImageControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/ImageControllerTest.java
@@ -14,6 +14,8 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import reactor.core.publisher.Mono;
+
 import java.util.HashSet;
 
 import static org.junit.Assert.assertEquals;
@@ -50,7 +52,7 @@ public class ImageControllerTest {
         RecipeCommand command = new RecipeCommand();
         command.setId("1");
 
-        when(recipeService.findCommandById(anyString())).thenReturn(command);
+        when(recipeService.findCommandById(anyString())).thenReturn(Mono.just(command));
 
         //when
         mockMvc.perform(get("/recipe/1/image"))
@@ -92,7 +94,7 @@ public class ImageControllerTest {
 
         command.setImage(bytesBoxed);
 
-        when(recipeService.findCommandById(anyString())).thenReturn(command);
+        when(recipeService.findCommandById(anyString())).thenReturn(Mono.just(command));
 
         //when
         MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage"))
